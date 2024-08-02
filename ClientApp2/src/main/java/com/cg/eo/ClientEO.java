e2c16f7c2542dbef3a06a96d060fda5011eec13e
Swarupa <chavanswarupa2299@gmail.com>
2024-08-02 09:54:51 +0530

    First commit

---
+++ ClientApp2/src/main/java/com/cg/eo/ClientEO.java
@@ -0,0 +1,45 @@
+package com.cg.eo;
+
+import lombok.Data;
+
+@Data
+public class ClientEO {
+	private long id;
+	private String emp_Name;
+	private String emp_EmailId;
+	private int emp_Age;
+	public ClientEO() {
+		super();
+		this.id = id;
+		this.emp_Name = emp_Name;
+		this.emp_EmailId = emp_EmailId;
+		this.emp_Age = emp_Age;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmp_Name() {
+		return emp_Name;
+	}
+	public void setEmp_Name(String emp_Name) {
+		this.emp_Name = emp_Name;
+	}
+	public String getEmp_EmailId() {
+		return emp_EmailId;
+	}
+	public void setEmp_EmailId(String emp_EmailId) {
+		this.emp_EmailId = emp_EmailId;
+	}
+	public int getEmp_Age() {
+		return emp_Age;
+	}
+	public void setEmp_Age(int emp_Age) {
+		this.emp_Age = emp_Age;
+	}
+	
+	
+
+}
